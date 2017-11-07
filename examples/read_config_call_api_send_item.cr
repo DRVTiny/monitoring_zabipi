e5e2742bf70b856ac3715c7e0cfea1b3032ad488
Andrey A. Konovalov <drvtiny@gmail.com>
2017-11-07 18:28:03 +0300

    Zabbix API version output

--- examples/read_config_call_api_send_item.cr
+++ examples/read_config_call_api_send_item.cr
@@ -19,19 +19,21 @@ begin
         p conf
 
 	zapi=Monitoring::Zabipi.new(conf["ZBX_URL"], conf["ZBX_LOGIN"], conf["ZBX_PASS"]);
+	printf(%[Zabbix API version implemented by %s is %s\n], conf["ZBX_URL"], zapi.version)
 
 	zans=zapi.do("trigger.get",{"triggerids"=>[110502],"expandDescription"=>1,"output"=>["description"]})
-
+	
 	# Output some field of the object with the desired index in result set
 	# ( .result is an Array of JSON::Any)
-	puts "description for the first found trigger: #{zans.result[0]["description"]}"
+	printf(%[Description for the first found trigger: <<%s>>\n], zans.result[0]["description"])
 
 	# Output result in JSON form
-	puts zans.result.map{ |t| [t["triggerid"], t["description"]] }.to_h.to_json
-	
-	puts "Sending data to Zabbix server..."
+	puts "Reformated result in JSON presentation:\n"+zans.result.map{ |t| [t["triggerid"], t["description"]] }.to_h.to_json
+	zsend_data=[{key: "test.metric", value: "hello!"}]
+	puts "Sending data to Zabbix server: <<#{zsend_data.to_json}>>"
 	zsend=Monitoring::Zabisend.new(conf["ZBX_SERVER"])
-	p zsend.req("test-zbx-snd",[{key: "test.metric", value: "hello!"}])
+	print "Zabbix sender answer:\n"
+	p zsend.req("test-zbx-snd",zsend_data)
 	
 	at_exit {
 		zapi.do("user.logout", [] of Int32)
