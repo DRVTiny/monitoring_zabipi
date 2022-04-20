164c7f7cebfe7e7c5b6a8772484f5445cdea6a98
Andrey A. Konovalov <drvtiny@gmail.com>
2022-04-20 16:37:52 +0300

    + possibility to get parameters directly from .sh-style config file

--- src/api/Zabipi.cr
+++ src/api/Zabipi.cr
@@ -1,6 +1,8 @@
 require "json"
 require "uri"
+require "openssl"
 require "http/client"
+
 
 module Monitoring
   DFLT_ZBX_API_RURL          = "/api_jsonrpc.php"
@@ -59,9 +61,12 @@ module Monitoring
   class ZAPIRequest
     getter :result
     @result : JSON::Any
-
+    
     def initialize(@ua : HTTP::Client, @zapi_url : URI, @raw_request : String)
+      pp @zapi_url
+      puts "#{@zapi_url.path.to_s}"
       resp = @ua.post(@zapi_url.path.to_s, headers: HTTP::Headers{"Content-Type" => "application/json"}, body: @raw_request)
+      pp resp
       raise HTTPException.new(resp.status) unless resp.status == HTTP::Status::OK
       zbxResp = JSON.parse(resp.body)
       if err = zbxResp["error"]?
@@ -75,57 +80,89 @@ module Monitoring
 
   class ZAPIAnswer
     @result : JSON::Any?
-
+    
     def initialize(@ua : HTTP::Client, @zapi_url : URI, @raw_request : String)
     end
 
     def result
-      r : JSON::Any
-      r = if @result.is_a?(JSON::Any)
-            @result || JSON.parse("[]")
-          else
-            ZAPIRequest.new(@ua, @zapi_url, @raw_request).result
-          end
-      @result = r
+      if (r = @result).is_a?(JSON::Any)
+        return r
+      else
+        @result = ZAPIRequest.new(@ua, @zapi_url, @raw_request).result
+      end
     end
   end
 
   class Zabipi
-    @version : String
-    @sAuthToken : String?
-    @oUserAgent : HTTP::Client
-    @oAPIUrl : URI
-    @sAPIRelUrl : String
-    getter :oUserAgent, :sAuthToken, :oAPIUrl, :version
-    property :debug
-
-    def initialize(apiUrl : String, login : String, password : String, @debug = false)
-      @oAPIUrl = URI.parse ( apiUrl =~ /\.[^\/.]+$/ ? apiUrl : ( apiUrl[-1] == '/' ? "" : "/" ) + "api_jsonrpc.php" )
-      urlScheme = (@oAPIUrl.scheme || "http").downcase
-      @oAPIUrl.scheme = urlScheme
-      @oAPIUrl.host = "localhost" if @oAPIUrl.host.nil?
-      @oAPIUrl.port = (@oAPIUrl.scheme == "https" ? 443 : 80) if @oAPIUrl.port.nil?
-      @sAPIRelUrl = @oAPIUrl.path || DFLT_ZBX_API_RURL
-      puts "zabipi init-d with: host=#{@oAPIUrl.host}, port=#{@oAPIUrl.port}, rurl=#{@sAPIRelUrl}" if @debug
-      @oUserAgent = HTTP::Client.new( @oAPIUrl )
+    @http_client : HTTP::Client
+    
+    getter auth_token : String?, api_url : URI, api_rel_url : String, version : String
+    property maybe_zbx_server : String
+    property debug : Bool
+    
+    def initialize(zenv_file : String)
+      conf = read_zenv(zenv_file)
+      initialize(
+        conf["ZBX_URL"],
+        conf["ZBX_LOGIN"],
+        conf["ZBX_PASS"],
+        debug: if conf["DEBUG"]? 
+          conf["DEBUG"] =~ /^(?i:[yt1])/ ? true : false
+        else
+          false
+        end,
+        zabbix_server: conf["ZBX_SERVER"]?
+      )
+    end
+    
+    def initialize(apiUrl : String, login : String, password : String, @debug = false, zabbix_server = nil)
+      @api_url = URI.parse ( apiUrl =~ /\.[^\/.]+$/ ? apiUrl : ( apiUrl[-1] == '/' ? "" : "/" ) + "api_jsonrpc.php" )
+      urlScheme = (@api_url.scheme || "http").downcase
+      @api_url.scheme = urlScheme
+      @api_url.host = "localhost" if @api_url.host.nil?
+      @maybe_zbx_server = zabbix_server ? zabbix_server : @api_url.host.not_nil!
+      @api_url.port = (@api_url.scheme == "https" ? 443 : 80) if @api_url.port.nil?
+      @api_rel_url = @api_url.path.size > 0 ? @api_url.path : (@api_url.path = DFLT_ZBX_API_RURL)
+      puts "zabipi init-d with: host=#{@api_url.host}, port=#{@api_url.port}, rurl=#{@api_rel_url}" if @debug
+      
+      tls_ctx = OpenSSL::SSL::Context::Client.new
+      tls_ctx.verify_mode = OpenSSL::SSL::VerifyMode::NONE
+      @http_client = HTTP::Client.new( @api_url, tls: tls_ctx )
+      puts "HERE!"
+      
       @version = ZAPIRequest
-        .new(@oUserAgent, @oAPIUrl, Hash{"jsonrpc" => "2.0", "method" => "apiinfo.version", "id" => 1, "params" => [] of UInt8}.to_json)
+        .new(@http_client, @api_url, Hash{"jsonrpc" => "2.0", "method" => "apiinfo.version", "id" => 1, "params" => [] of UInt8}.to_json)
         .result.as_s
+      
       puts "zapiVersion=#{@version}" if @debug
-      @sAuthToken = ZAPIRequest
-        .new(@oUserAgent, @oAPIUrl, Hash{"jsonrpc" => "2.0", "method" => "user.login", "id" => 1, "params" => {"password" => password, "user" => login}}.to_json)
+      @auth_token = ZAPIRequest
+        .new(@http_client, @api_url, Hash{"jsonrpc" => "2.0", "method" => "user.login", "id" => 1, "params" => {"password" => password, "user" => login}}.to_json)
         .result.as_s
-      puts "authToken=#{@sAuthToken}" if @debug
+      puts "authToken=#{@auth_token}" if @debug
     end
 
     def do(method : String, pars : (Hash | Array) = [] of UInt8)
-      return ZAPIAnswer.new(@oUserAgent, @oAPIUrl, Hash{
+      return ZAPIAnswer.new(@http_client, @api_url, Hash{
         "jsonrpc" => "2.0",
         "method"  => method,
         "id"      => 0,
         "params"  => pars,
-        "auth"    => @sAuthToken,
+        "auth"    => @auth_token,
       }.to_json)
+    end
+    
+    private def read_zenv(zenv_path : String) : Hash(String, String)
+      rows = File.read_lines(zenv_path)
+      rows.reject! { |line| line =~ /^\s*(?:#.*)?$/ }
+      conf = rows.map do |line|
+          if line.match(/^([^#=\s]+)\s*=\s*(?:(?<Q>["'`])((?:(?!\k<Q>|\\).|\\.)*)\k<Q>)(?:\s+(?:#.*)?)?$/)
+            [$1, $3]
+          elsif 	line.match(/^([^#=\s]+)\s*=\s*([^#\s"']+)(?:\s+(?:#.*)?)?/)
+            [$1, $2]
+          else
+            raise "invalid-formatted config line: #{line}"
+          end
+      end.to_h
     end
   end
 end
